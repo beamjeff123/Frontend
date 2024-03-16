0a84b35dc2082c5d6ff51a9e6e91b0facd48f448
beamjeff123 <navasoverzone@gmail.com>
2024-03-16 14:57:35 +0700

    o

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  const textInput = <input type="text" />;
+  const okButton = <button> click </button>;
+  return (
+
+    <div className="App">
+      
+      <p>{textInput}</p>
+      <p>{okButton}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ Hello.js
@@ -0,0 +1,16 @@
+function Hello() {
+    let name = "korawit orkphol"
+    return (
+        <>
+            <h1>
+                Hello World, {name+' dsaa'}
+            </h1>
+            <h2>
+                Love
+            </h2>
+        </>
+
+    )
+}
+
+export default Hello;
